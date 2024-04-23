79f3cf31fb710bbba1a8f71c7e11e2809447b887
SushiRoll53 <bryan@pif.co>
2024-04-23 11:58:57 -0600

    Add component practice

---
+++ src/pages/shoppingList.tsx
@@ -0,0 +1,32 @@
+
+function ShoppingList() {
+    const products = [
+        { name: "Apple", price: 1.00 },
+        { name: "Banana", price: 0.60 },
+        { name: "Carrot", price: 0.30 },
+    ];
+
+    return (
+        <div>
+            <header>
+                <h1>Welcome to My Page</h1>
+                <nav>
+                </nav>
+            </header>
+            <section>
+                {products.map((product, key) => (
+                    <div key={key}>
+                        <h3>{product.name}</h3>
+                        <p>${product.price.toFixed(2)}</p>
+                    </div>
+                ))}
+            </section>
+
+            <footer>
+                <p>© 2024 My Website</p>
+            </footer>
+        </div>
+    );
+};
+
+export default ShoppingList;
